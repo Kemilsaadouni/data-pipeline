c2879b88a2abe1e79381b84530a3179b41129107
Kemilsaadouni <kemil.saadouni@epitech.digital>
2025-04-22 16:38:54 +0200

    Add env variables

--- preprocessing/app.py
+++ preprocessing/app.py
@@ -1,7 +1,18 @@
+import os
 import pandas as pd
 import psycopg2
 from sqlalchemy import create_engine
 import time
+from dotenv import load_dotenv
+
+# Charge les variables d'environnement depuis le fichier .env
+load_dotenv()
+
+user = os.getenv("POSTGRES_USER")
+password = os.getenv("POSTGRES_PASSWORD")
+host = os.getenv("POSTGRES_HOST")
+port = 5432
+dbname = os.getenv("POSTGRES_DB")
 
 time.sleep(10)
 df = pd.read_csv('/data/iris.csv')
@@ -23,7 +34,7 @@ if len(df) < initial_len:
 df = df[['sepal_length', 'sepal_width', 'species']]
 
 # Connexion et envoi dans la base de données
-engine = create_engine('postgresql://user:password@db:5432/iris')
+engine = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{dbname}')
 df.to_sql('iris_data', engine, if_exists='replace', index=False)
 
 print("✅ Données nettoyées et importées avec succès dans PostgreSQL.")
